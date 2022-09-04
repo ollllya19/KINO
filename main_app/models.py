e8c70c3a477544cff7a372cf8076e908a1688e1d
Ollly19 <79220370+ollllya19@users.noreply.github.com>
2022-09-04 15:15:09 +0500

    update

--- main_app/models.py
+++ main_app/models.py
@@ -11,12 +11,12 @@ class User(models.Model):
     phone = models.CharField(max_length=64, unique=True, null=True)
     password = models.CharField(max_length=64, null=True)
 
-    def __str__(self):
-        return f"id: {self.id} Логин: {self.phone}  Пароль: {self.password}"
-
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+        
+    def __str__(self):
+        return f"id: {self.id} Логин: {self.phone}  Пароль: {self.password}"
 
 
 class Film(models.Model):
@@ -30,41 +30,41 @@ class Film(models.Model):
     overview = models.TextField(null = True)
     director = models.CharField(max_length=50, null = True)
 
-    def __str__(self):
-        return f"Название: {self.title}, жанр: {self.genre}, фото: {self.photo}, ревью: {self.overview}"
-
     class Meta:
             verbose_name = "Фильм"
             verbose_name_plural = "Фильмы"
             ordering = ['title']
+            
+    def __str__(self):
+        return f"Название: {self.title}, жанр: {self.genre}, фото: {self.photo}, ревью: {self.overview}"
 
 
 class FilmPurchase(models.Model):
-    userID = models.ForeignKey(User, on_delete=models.CASCADE)
-    filmID = models.ForeignKey(Film, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
     date = models.DateField(null=True, blank=True, auto_now=True)   
     cheque = models.FileField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     card = models.CharField(null=True, blank=True, max_length=64)
 
-    def __str__(self):
-        return f"{self.userID}: {self.filmID}"
-
     class Meta:
             verbose_name = "Покупка"
             verbose_name_plural = "Покупки"
             ordering = ['date']
+            
+    def __str__(self):
+        return f"{self.user}: {self.film}"
 
 
 class FilmLookUp(models.Model):
-    userID = models.ForeignKey(User, on_delete=models.CASCADE)
-    filmID = models.ForeignKey(Film, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
     datetime = models.DateTimeField(null=True, blank=True, auto_now=True)   
-
-    def __str__(self):
-        return f"{self.userID}: {self.filmID}"
 
     class Meta:
             verbose_name = "Просмотр фильма"
             verbose_name_plural = "Просмотры фильмов"
             ordering = ['datetime']
+            
+    def __str__(self):
+        return f"{self.user}: {self.film}"
